134c036376ff14bbf28faeb1cd39f12d9324336d
Jason Field <jason@avon-lea.co.uk>
2020-05-15 17:44:31 +0100

    Add rspec matcher

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
-mkdir _matchers
-cp /cookstyle.json _matchers/cookstyle.json
-echo "##[add-matcher]_matchers/cookstyle.json"
+mkdir ._actionshub_problem-matchers
+cp /cookstyle.json ._actionshub_problem-matchers/cookstyle.json
+cp /rspec.json ._actionshub_problem-matchers/rspec.json
+echo "##[add-matcher]._actionshub_problem-matchers/cookstyle.json"
+echo "##[add-matcher]._actionshub_problem-matchers/rspec.json"
 chef exec delivery local all
