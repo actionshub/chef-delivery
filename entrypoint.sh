7e40ff675b0bf50312f2316433acd6e1fa2aac9e
Lance Albertson <ramereth@gmail.com>
2020-09-28 09:35:44 -0700

    Add optional `gems` input (#7)
    Sometimes cookbook ChefSpec tests require additional gem(s) to be installed to
    test the code properly. This allows users to set what gem(s) to to be installed.
    
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,8 @@ cp /cookstyle.json ._actionshub_problem-matchers/cookstyle.json
 cp /rspec.json ._actionshub_problem-matchers/rspec.json
 echo "##[add-matcher]._actionshub_problem-matchers/cookstyle.json"
 echo "##[add-matcher]._actionshub_problem-matchers/rspec.json"
+if [ -n "${INPUT_GEMS}" ] ; then
+  echo "Installing gem(s): ${INPUT_GEMS}"
+  chef gem install -N "${INPUT_GEMS}"
+fi
 chef exec delivery local all
