77402bc82d41a24722ab08820be316134a53dc7c
Jason Field <jason@avon-lea.co.uk>
2020-11-28 13:41:58 +0000

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,8 @@ cp /cookstyle.json ._actionshub_problem-matchers/cookstyle.json
 cp /rspec.json ._actionshub_problem-matchers/rspec.json
 echo "##[add-matcher]._actionshub_problem-matchers/cookstyle.json"
 echo "##[add-matcher]._actionshub_problem-matchers/rspec.json"
-<<<<<<< HEAD
-=======
 if [ -n "${INPUT_GEMS}" ] ; then
   echo "Installing gem(s): ${INPUT_GEMS}"
   chef gem install -N "${INPUT_GEMS}"
 fi
->>>>>>> 7e40ff675b0bf50312f2316433acd6e1fa2aac9e
 chef exec delivery local all
