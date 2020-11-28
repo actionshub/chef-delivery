b736c6bb851440fbd247626871f9ba5d8256b9b8
Jason Field <jason@avon-lea.co.uk>
2020-11-28 13:37:46 +0000

    Add Deprection warning for master

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
+echo "::warning ::Github Action: actionshub/chef-delivery has migrated to the main branch as default, the master branch will be removed"
+
 mkdir ._actionshub_problem-matchers
 cp /cookstyle.json ._actionshub_problem-matchers/cookstyle.json
 cp /rspec.json ._actionshub_problem-matchers/rspec.json
